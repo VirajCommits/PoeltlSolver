61d6c48264a41c29d29ada3362d62b724fa83cde
VirajCommits <virajmurab567@yahoo.com>
2024-10-23 03:06:12 -0600

    Revert "Revert "deleted venv""
    This reverts commit e24733da93a3f64f1723d06e05db18bf7cf542bc.

--- poeltl/src/PlayerGuesser.js
+++ poeltl/src/PlayerGuesser.js
@@ -2,14 +2,11 @@ import React , {useState} from 'react'
 
 function PlayerGuesser() {
 
-
-
     const [suggestions, setSuggestions] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const handleSearchPlayers = async () => {
-      console.log("HANDLING SEARCHING OF PLAYERS ")
         setLoading(true);
         setError(null);
 
@@ -43,6 +40,7 @@ function PlayerGuesser() {
                     },
                     body: JSON.stringify(currentPlayerStats),
                 });
+            console.log(response)
             
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
